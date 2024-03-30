75156e0df9e385e5d36c924d07f75a2fa120dcc8
TwentyTrash7233 <101785568+TwentyTrash7233@users.noreply.github.com>
2024-03-29 19:08:27 -0700

    Update AimAssistHack.java

--- src/main/java/net/wurstclient/hacks/AimAssistHack.java
+++ src/main/java/net/wurstclient/hacks/AimAssistHack.java
@@ -34,7 +34,7 @@ public final class AimAssistHack extends Hack
 	implements UpdateListener, RenderListener
 {
 	private final SliderSetting range =
-		new SliderSetting("Range", 4.5, 1, 60, 0.05, ValueDisplay.DECIMAL);
+		new SliderSetting("Range", 4.5, 1, 100, 0.05, ValueDisplay.DECIMAL);
 	
 	private final SliderSetting rotationSpeed =
 		new SliderSetting("Rotation Speed", 600, 10, 3600, 10,
